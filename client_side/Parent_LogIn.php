bc7e82fad3d72f5e518b6cbe57edf651a350b103
Lovelyy <lovelycruz.1317@gmail.com>
2025-04-04 13:32:15 +0800

    Update mo muna dabid

---
+++ client_side/Parent_LogIn.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parent LogIn</title> 
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/Parent_LogIn.css">
+</head>
+
+<body>
+    <div class="main">
+
+        <div class="header">
+                <h2>Lucena South II Elementary School</h2>
+        </div>
+
+        <div class="form-container">
+
+        </div>
+ 
+    </div>
+</body>
+</html>
